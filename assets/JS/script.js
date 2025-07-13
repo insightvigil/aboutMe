928aa287061a6fead32ce3500972e34ecb3adeef
Adrián Vigil <insightvigil@gmail.com>
2025-07-12 20:00:21 -0600

    dark mode

---
+++ assets/JS/script.js
@@ -0,0 +1,14 @@
+/*const profileCard = document.getElementsByClassName('profile-card')[0];
+console.log(profileCard)
+
+const btnTheme = document.getElementById('switch-btn');
+
+const switchTheme = () =>
+{
+    profileCard.classList.toggle('light')
+}
+
+
+btnTheme.addEventListener('click', switchTheme)
+
+*/
