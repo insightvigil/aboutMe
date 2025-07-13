fb7d81b14ba335481fe4b1b98fd7cba32bdc8cb5
Adrián Vigil <insightvigil@gmail.com>
2025-07-13 07:11:47 -0600

    Darkmode Finished

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,14 +1,34 @@
-/*const profileCard = document.getElementsByClassName('profile-card')[0];
-console.log(profileCard)
+const themeSwitcher = document.getElementById('theme-switcher');
 
-const btnTheme = document.getElementById('switch-btn');
+//Light Mode 
 
-const switchTheme = () =>
-{
-    profileCard.classList.toggle('light')
+//First we targeting the elements that we will change in the button 
+const lightMode = () => {
+    themeSwitcher.children[0].textContent = 'Light Mode'
+    //We change the icon throught its class
+    themeSwitcher.children[1].classList.replace('fa-moon', 'fa-sun')
 }
 
+const darkMode = () => {
+    themeSwitcher.children[0].textContent = 'Dark Mode'
+    //We change the icon throught its class
+    themeSwitcher.children[1].classList.replace('fa-sun', 'fa-moon')
+}
 
-btnTheme.addEventListener('click', switchTheme)
+// Switche Theme
 
-*/
+const switchTheme = () => {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    if (!currentTheme || currentTheme === 'dark') {
+        document.documentElement.setAttribute('data-theme', 'light')
+        lightMode();
+    }
+
+    else {
+        document.documentElement.setAttribute('data-theme', 'dark')
+        darkMode();
+    }
+}
+
+//Evenet Listener 
+themeSwitcher.addEventListener('click', switchTheme);
